1f0c30d401920fe5ddc86be049d5f59f2107fb50
Connor Peck <connorjamespeck@gmail.com>
2022-05-03 08:37:50 -0500

    cleaned up and got rid of redundencies

--- server/modules/calculate.js
+++ server/modules/calculate.js
@@ -4,21 +4,24 @@ const historyOfCalculations_requestedByUser = [];
 let resultOfCurrentCalculation;
 
 
-
+// a get request from a generic route 
 router.get( '/', (req, res) =>{
     console.log( '/calculate GET' );
+    // the response from the request is a new object containing history and result
     res.send({history: historyOfCalculations_requestedByUser, result: resultOfCurrentCalculation});
 });
 
 router.post('/', (req, res)=> {
   console.log('/calculate POST', req.body);
   updateHistory(req.body, calculate(req.body));
+  // runs the function updateHistory
+  // with the information inputted and the calculated information
   res.sendStatus(200);
 })
 
 module.exports = router;
 
-//  start of math operator logic 
+// doing the actual math
 
 function calculate (calculation_requestedByUser){
   let resultOfCalculation_requestedByUser; 
@@ -40,7 +43,7 @@ function calculate (calculation_requestedByUser){
 }
 return resultOfCalculation_requestedByUser;
 } // end calculations 
-
+// update history to send back to the client
 function updateHistory (calculation_requestedByUser, resultOfCalculation_requestedByUser ){
 let recordOfCalculation = {
 input1: calculation_requestedByUser.input1,
@@ -48,6 +51,7 @@ input2: calculation_requestedByUser.input2,
 operation: calculation_requestedByUser.operation,
 answer: resultOfCalculation_requestedByUser
 }
+// pushing the record into const array historyOfCalculations_requestedByUser
 historyOfCalculations_requestedByUser.push(recordOfCalculation);
 resultOfCurrentCalculation= resultOfCalculation_requestedByUser;
 
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -36,27 +36,31 @@ function requestCalculation(){
         console.log('back from POST', response);
         // getting a response back from the server
         requestHistoryAndResultOfCalculation();
+        // running a function that gets info from the server
+        // and runs the function updateDom
     }).catch ( function ( err ){
         console.log(err);
         alert('error in numbersToBeCalculated ')
-    })
-
+    })  // catch if there is an error
 } // end requestCalculation
 
 function requestHistoryAndResultOfCalculation(){
     $.ajax({
         method: 'GET',
         url: '/calculate'
+        // getting data from the server via GET request
     }).then (function (response){
         updateDom(response);
+        // gets back a response containing an object with the history 
+        // and the result of the calculation
         console.log('this is the response', response);
 } 
     )}// end getNumbers
+    // update dom with paramater of the response from get request
 function updateDom (historyAndResultOfCalculation) {
     const el = $('#calculationsOut');
     el.empty();
-    // el.append(`<li>${historyAndResultOfCalculation.result}</li>`);
-    // el.append(historyAndResultOfCalculation.history[1].input1);
+    // loop and update the dom wiht the right operator sign
     for (let i=0; i<historyAndResultOfCalculation.history.length; i++){
         let operationSign = '';
         if (historyAndResultOfCalculation.history[ i ].operation === 'add'){
@@ -70,13 +74,15 @@ function updateDom (historyAndResultOfCalculation) {
         }
         else if (historyAndResultOfCalculation.history[ i ].operation === 'divide'){
             operationSign = '/';
-        }
+        } // append a list item with the input numbers and 
+        // chosen operator sign
     el.append(`<li> 
     ${historyAndResultOfCalculation.history[ i ].input1 } 
     ${operationSign}
     ${historyAndResultOfCalculation.history[ i ].input2} = 
     ${historyAndResultOfCalculation.history[ i ].answer} </li>`);
         }
+        // appending the answer to the dom
     const answerToDom= $('#answer');
     answerToDom.empty();
     answerToDom.append(historyAndResultOfCalculation.result);
